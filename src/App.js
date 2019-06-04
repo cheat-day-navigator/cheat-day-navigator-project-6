aee36b5ff80f4695d4f1e8c6ecbbf5d269ad3d69
katbosnic <katbosnic@gmail.com>
2019-06-04 16:41:24 -0400

    basic JSX outline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <h4>Superfood Navigation</h4>
+            <ul className="menu">
+              <li>Saved Items</li>
+              <ul className="drop-down">
+                <li>Saved Item</li>
+              </ul>
+            </ul>
+          </nav>
+          <h1>Superfood Navigation</h1>
+          <h3>Learn about all your favourite foods!</h3>
+        </header>
+        <main>
+          <form>
+            <label htmlFor="search">Look up your favourite foods!</label>
+            <input type="text" id="search" placeholder="e.g. Chicken nuggets" />
+            <div className="buttons">
+              <button type="submit">Compare</button>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+          <div className="gallery-field">
+            <button>Back</button>
+            <div className="item-card">
+              <img src="https://fillmurray.com/200/300" alt="" />
+              <button>Read More</button>
+              <div className="nutrition-card">
+                <h3>Nutrition Facts</h3>
+                <p className="line">1 serving</p>
+                <ul>
+                  <li><p>Vitamin A</p><p>value</p></li>
+                  <li><p>Vitamin A</p><p>value</p></li>
+                  <li><p>Vitamin A</p><p>value</p></li>
+                  <li><p>Vitamin A</p><p>value</p></li>
+                </ul>
+              </div>
+            </div>
+            <button>Next</button>
+          </div>
+        </main>
+        <footer>
+          <p>Made by Inder, Chitra, Paul, and Kat using the Nutrionix API</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
