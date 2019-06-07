af5c1d2ff571e1da50df8d1b23444c4e02a6e90d
lbCyber <paul@lintbox.com>
2019-06-07 16:03:30 -0400

    please work - macronutrients

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
       }
     }).then(result => {
       this.setState({ macroNutrients: result.data })
-<<<<<<< HEAD
       console.log(result.data)
     })
   }
@@ -69,47 +68,6 @@ class App extends Component {
       </div>
     );
   }
-=======
-    })
-}
-
-callBackData = (d) => {
-  this.setState({ nutriData: d })
-}
-
-
-showNutritionCard = () => {
-  this.setState({
-    nutritionVisible: true
-  })
-}
-
-
-
-
-
-
-render() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <InputForm
-          data={this.callBackData}
-          toggleCard={this.showNutritionCard}
-          value={this.state.userInput}
-        />
-        {this.state.nutritionVisible ? <NutritionCard
-          data={this.state.nutriData.common} value={this.state.userInput} nutrients={this.state.macroNutrients}
-        /> : null}
-      </main>
-      <footer>
-        <p>Made by Inder, Chitra, Paul, and Kat using the Nutrionix API</p>
-      </footer>
-    </div>
-  );
-}
->>>>>>> 61d0b1995ee507b1644d8c43d68385ea52dfe343
 }
 
 export default App;
