61d0b1995ee507b1644d8c43d68385ea52dfe343
lbCyber <paul@lintbox.com>
2019-06-07 14:46:42 -0400

    macronutrient array grabbed, passed to nutritioncard

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import InputForm from './localComponents/inputForm';
 import Header from './localComponents/Header.js';
 import NutritionCard from './NutritionCard.js';
+import APIData from './localComponents/apiData'
+import axios from 'axios'
 
-class App extends Component {
+class App extends Component { 
   constructor() {
     super();
     // Create an empty initial state;
@@ -12,46 +14,62 @@ class App extends Component {
       nutriData: {},
       userInput: '',
       nutritionVisible: false,
-
+      macroNutrients: {}
     }
   }
 
-  callBackData = (d) => {
-    this.setState({ nutriData: d })
-  }
-
-
-  showNutritionCard = () => {
-    this.setState({
-      nutritionVisible: true
+  componentDidMount() {
+    const appKeyGrab = APIData.appKey
+    const appIdGrab = APIData.appId
+    axios({
+      url: 'https://trackapi.nutritionix.com/v2/utils/nutrients',
+      method: `GET`,
+      dataResponse: `json`,
+      headers: {
+        'x-app-id': appIdGrab,
+        'x-app-key': appKeyGrab
+      }
+    }).then(result => {
+      this.setState({ macroNutrients: result.data })
     })
-  }
+}
+
+callBackData = (d) => {
+  this.setState({ nutriData: d })
+}
+
+
+showNutritionCard = () => {
+  this.setState({
+    nutritionVisible: true
+  })
+}
 
 
 
 
 
 
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <main>
-          <InputForm
-            data={this.callBackData}
-            toggleCard={this.showNutritionCard}
-            value={this.state.userInput}  
-          />
-          {this.state.nutritionVisible ? <NutritionCard
-            data={this.state.nutriData.common} value={this.state.userInput}
-          /> : null}
-        </main>
-        <footer>
-          <p>Made by Inder, Chitra, Paul, and Kat using the Nutrionix API</p>
-        </footer>
-      </div>
-    );
-  }
+render() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <InputForm
+          data={this.callBackData}
+          toggleCard={this.showNutritionCard}
+          value={this.state.userInput}
+        />
+        {this.state.nutritionVisible ? <NutritionCard
+          data={this.state.nutriData.common} value={this.state.userInput} nutrients={this.state.macroNutrients}
+        /> : null}
+      </main>
+      <footer>
+        <p>Made by Inder, Chitra, Paul, and Kat using the Nutrionix API</p>
+      </footer>
+    </div>
+  );
+}
 }
 
 export default App;
