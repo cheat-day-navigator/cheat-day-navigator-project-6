1631829e24bc9d7e340522fb7665f3f3de694936
lbCyber <paul@lintbox.com>
2019-06-07 14:20:15 -0400

    Adjusted modal style

--- src/localComponents/loadingModal.js
+++ src/localComponents/loadingModal.js
@@ -1,32 +1,34 @@
-import react from 'react'
-import loadingStyle from '../styles/loading.css'
+import React from 'react'
+import '../styles/loading.css'
 
 const LoadingModal = () => {
   return (
-    <div class="modal-loading">
-      <div class="container-loading">
-        <div class="bars-loading">
-          <div class="bar-loading bar1-loading"></div>
-          <div class="bar-loading bar2-loading"></div>
-          <div class="bar-loading bar3-loading"></div>
-          <div class="bar-loading bar4-loading"></div>
-          <div class="bar-loading bar5-loading"></div>
-          <div class="bar-loading bar6-loading"></div>
-          <div class="bar-loading bar7-loading"></div>
-          <div class="bar-loading bar8-loading"></div>
-          <div class="bar-loading bar9-loading"></div>
-          <div class="bar-loading bar10-loading"></div>
-          <div class="bar-loading bar11-loading"></div>
-          <div class="bar-loading bar12-loading"></div>
-          <div class="bar-loading bar13-loading"></div>
-          <div class="bar-loading bar14-loading"></div>
-          <div class="bar-loading bar15-loading"></div>
+    <div className="modal-loading">
+      <div className="container-loading">
+        <div className="bars-loading">
+          <div className="bar-loading bar1-loading"></div>
+          <div className="bar-loading bar2-loading"></div>
+          <div className="bar-loading bar3-loading"></div>
+          <div className="bar-loading bar4-loading"></div>
+          <div className="bar-loading bar5-loading"></div>
+          <div className="bar-loading bar6-loading"></div>
+          <div className="bar-loading bar7-loading"></div>
+          <div className="bar-loading bar8-loading"></div>
+          <div className="bar-loading bar9-loading"></div>
+          <div className="bar-loading bar10-loading"></div>
+          <div className="bar-loading bar11-loading"></div>
+          <div className="bar-loading bar12-loading"></div>
+          <div className="bar-loading bar13-loading"></div>
+          <div className="bar-loading bar14-loading"></div>
+          <div className="bar-loading bar15-loading"></div>
         </div>
-        <p class="text-loading">Loading <span class="dot-loading dot1-loading">. </span><span
-          class="dot-loading dot2-loading">. </span><span class="dot-loading dot3-loading">.
+        <p className="text-loading">Loading <span className="dot-loading dot1-loading">. </span><span
+          className="dot-loading dot2-loading">. </span><span className="dot-loading dot3-loading">.
         </span>
         </p>
       </div>
     </div>
   )
 }
+
+export default LoadingModal
