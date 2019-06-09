d2077cf01378b7f5c5bb1e89ea13f20053cca7d1
lbCyber <paul@lintbox.com>
2019-06-09 00:19:39 -0400

    Refactored axios calls into a simple function

---
+++ src/globalComponents/makeCall.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import APIData from '../localComponents/apiData'
+import Swal from 'sweetalert2';
+
+// A global, multipurpose axios function that can handle multiple different kinds of API calls
+
+const MakeCall = (searchtype = `simpleSearch`, appQuery = `chicken nuggets`) => {
+  const appKeyGrab = APIData.appKey // grab local key (will remove on build)
+  const appIdGrab = APIData.appId // grab local id (will remove on build)
+  let urlType = '' // Determining what kind of API call to do
+  let methodType = `GET` // Defaulting API method to GET
+  let parameters = {} // Determining which params (if applicable) to add to API call
+
+  if (searchtype === `macroNutrients`) { // Macronutrient call
+    urlType = `https://trackapi.nutritionix.com/v2/utils/nutrients`
+
+  } else if (searchtype === `simpleSearch`) { // Regular food search
+    urlType = `https://trackapi.nutritionix.com/v2/search/instant`
+    parameters = {
+      'query': appQuery
+    }
+
+  } else if (searchtype === `readMore`) { // Information call for specific food result
+    urlType = 'https://trackapi.nutritionix.com/v2/search/item'
+    parameters = {
+      'tag_id': appQuery
+    }
+  }
+  return (
+    axios({
+      method: methodType,
+      url: urlType,
+      dataResponse: `json`,
+      headers: {
+        'x-app-id': appIdGrab,
+        'x-app-key': appKeyGrab
+      },
+      params: parameters
+    }).then(response => {
+      return response.data
+    }).catch(error => {
+      console.log(error)
+      Swal.fire({ // If none of these match, something went wrong on your end!
+        title: 'Oops!',
+        text: `Something went wrong. Please wait a moment, and try your search again!`,
+        type: 'error',
+        confirmButtonText: 'Okay'
+      })
+    })
+  )
+}
+
+
+export default MakeCall
