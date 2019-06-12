fcaccb8ccc4c784d745ab9186a641d779b202330
katbosnic <katbosnic@gmail.com>
2019-06-11 20:51:29 -0400

    Updates

---
+++ precache-manifest.65bb41cb50d9d95a2d31362bd3ca06d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c680ee17ce63d3da9aa29b83e30ab8a5",
+    "url": "/superfoods-navigator-project-6/index.html"
+  },
+  {
+    "revision": "048e14db1e34afd3db66",
+    "url": "/superfoods-navigator-project-6/static/css/main.a38044e5.chunk.css"
+  },
+  {
+    "revision": "e10d229a532fb8e7eed8",
+    "url": "/superfoods-navigator-project-6/static/js/2.ede5cc61.chunk.js"
+  },
+  {
+    "revision": "048e14db1e34afd3db66",
+    "url": "/superfoods-navigator-project-6/static/js/main.7527bcf6.chunk.js"
+  },
+  {
+    "revision": "00c9bcbf1205199d9fa8",
+    "url": "/superfoods-navigator-project-6/static/js/runtime~main.075e4109.js"
+  },
+  {
+    "revision": "c506b0e86ef4cdf5c6a2f35a06bd78e3",
+    "url": "/superfoods-navigator-project-6/static/media/header-image.c506b0e8.jpg"
+  }
+]);
